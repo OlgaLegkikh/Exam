9596455d7cd1a22829677590d7eb4470e01dec52
OlgaLegkikh <23OShe@gmail.com>
2023-08-17 01:18:59 +0300

    Task 4 start

---
+++ Task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Разработайте класс содержащий произвольный публичный метод и событие.
+// После 10 вызовов метода у любых экземпляров разработанного класса
+// необходимо вызвать событие и информировать об этом пользователя.
+// Обработку события произвести либо при помощи анонимного метода, либо при помощи лямбда-вырожения.
+
+delegate int Sum(int number);
+
+class Program
+{
+    static Sum SomeVar()
+    {
+        int result = 0;
+
+        // Вызов анонимного метода
+        Sum del = delegate (int number)
+        {
+            for (int i = 0; i <= number; i++)
+                result += i;
+            return result;
+        };
+        return del;
+    }
+
+    static void Main()
+    {
+        Sum del1 = SomeVar();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Cумма {0} равна: {1}", i, del1(i));
+        }
+
+        Console.ReadLine();
+    }
+}
