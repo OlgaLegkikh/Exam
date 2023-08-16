5f38c5b983472a1a993bda0c2a087a95672358bd
OlgaLegkikh <23OShe@gmail.com>
2023-08-17 01:18:38 +0300

    Task 5 done

---
+++ Task_5/InteractionsWithUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+
+namespace Task_5
+{
+	public class InteractionsWithUser
+	{
+		public InteractionsWithUser()
+		{
+		}
+		public int OperationChoose()
+		{
+			try
+			{
+				Console.WriteLine("Выберите операцию: \n 1 - сложение, \n 2 - вычитание \n 3 - умножение \n 4 - деление \n 5 - факториал, \n 6 - возведение в степень \n 0 - выйти из программы");
+				return Convert.ToInt32(Console.ReadLine());
+			}
+			catch (NullReferenceException)
+			{
+				Console.WriteLine("Выберите операцию: ");
+				return OperationChoose();
+			}
+			catch (FormatException)
+			{
+				Console.WriteLine("Введите число: ");
+				return OperationChoose();
+			}
+		}
+
+		public double NumberAsk(string promt)
+		{
+			try
+			{
+				Console.WriteLine(promt);
+				return double.Parse(s: Console.ReadLine(), CultureInfo.InvariantCulture);
+			}
+			catch (ArgumentNullException)
+			{
+				return NumberAsk(promt);
+            }
+			catch (FormatException)
+			{
+				return NumberAsk(promt);
+			}
+		}
+
+		public void ShowResult(double result)
+		{
+			Console.WriteLine($"Результат: {result}");
+			Console.WriteLine(Environment.NewLine);
+		}
+	}
+}
+
---
+++ Task_5/Program.cs
@@ -0,0 +1,36 @@
+﻿// Разработайте класс для выполнения математических функций 
+// факториал,
+// возведение в степень.
+// Необходимо предусмотреть защиту от некорректного ввода данных,
+// а также обработку исключетельных ситуаций.
+// При возникновении исключительной ситуации пользователь должен получить
+// соответствующее сообщение с описанием исключительной ситуации.
+// Не забывайте про принципы ООП.
+
+
+using Task_5;
+
+var askUser = new InteractionsWithUser();
+var calculator = new Calculator();
+bool isOn = true;
+
+while (isOn)
+{
+    var operation = askUser.OperationChoose();
+    if ( operation == 0)
+    {
+        isOn = false;
+        
+    }
+    else if(operation == 5)
+    {
+        askUser.ShowResult(calculator.DoOperation(askUser.NumberAsk("Введите число: "), (Operation)operation));
+    }
+    else if(operation == 6){
+        askUser.ShowResult(calculator.DoOperation(askUser.NumberAsk("Введите число: "), (Operation)operation, askUser.NumberAsk("Введите cтепень: ")));
+    }
+    else
+    {
+        askUser.ShowResult(calculator.DoOperation(askUser.NumberAsk("Введите первое число: "), (Operation)operation, askUser.NumberAsk("Введите второе число: ")));
+    }
+}
